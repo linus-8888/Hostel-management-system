333b55a4ed24566a83e5de8fd59bb00c94beb984
linus-8888 <linuslinus020@gmail.com>
2024-09-16 11:45:53 +0300

    Add files via upload
    This is a hostel management system by a student pursuing computer science. 
    for educational purposes only.

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿create table Newstude1(
+id int identity(1,1) primary key,
+mobile bigint not null,
+name varchar (250) not null,
+fname varchar (250) not null,
+mnamme varchar (250) not null,
+email varchar (250) not null,
+adminNo varchar (250) not null,
+roomNo varchar (250) not null,
+foreign key (roomNo) references rooms(roomNo)
+)
---
+++ SQLQuery5.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Employees(
+id int identity(1,1) Primary Key,
+EmpMobile bigint not null,
+EmpName varchar (250) not null,
+EmpEmail varchar (250) not null,
+EmpAge varchar (250) not null,
+Dept varchar (250) not null,
+)
+
